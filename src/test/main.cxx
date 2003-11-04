811415bf587871606b98be7a8c0cf7b1f794acfa
jchiang <>
2003-11-04 04:38:47 +0000

    working version of simple test program

--- src/test/main.cxx
+++ src/test/main.cxx
@@ -1,22 +1,36 @@
+#include <cstring>
+
 #include "hoops/hoops_limits.h"
 #include "hoops/hoops.h"
 #include "hoops/hoops_pil_factory.h"
 #include "hoops/hoops_pil.h"
 
 int main(int argc, char* argv[]) {
-   hoops::IParFile * pf = hoops::PILParFileFactory().NewIParFile(argv[0]);
+   std::string filename("file.par");
+   hoops::IParFile * pf 
+      = hoops::PILParFileFactory().NewIParFile(filename.c_str());
 
-   pf->Load();
+   pf->Load();  // should be in constructor?
 
+   char *lame_char_array[1];
+   lame_char_array[0] = strdup(filename.c_str());
    hoops::IParPrompt * prompter 
-      = hoops::PILParPromptFactory().NewIParPrompt(argc-1, argv+1);
+      = hoops::PILParPromptFactory().NewIParPrompt(1, lame_char_array);
 
    prompter->Prompt();
 
    hoops::IParGroup & pg = prompter->Group();
 
-   pg["string_par"] = "This is a string parameter";
-   double d = pg["double_par"];
+   string my_string = pg["string_par"];
+   double my_double = pg["double_par"];
+
+   std::cout << my_string << "\n"
+             << my_double << std::endl;
+
+   pf->Save();   // should be in destructor?
+
+   delete pf;
+   delete prompter;
 }
 
       
