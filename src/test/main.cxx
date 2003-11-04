2d649abd0d22d9fcead3c9bdc048ecc025df6ba0
jchiang <>
2003-11-04 17:20:06 +0000

    add bits to compile main.cxx on windows

--- src/test/main.cxx
+++ src/test/main.cxx
@@ -1,32 +1,38 @@
 #include <cstring>
+#include <iostream>
+#include <string>
 
 #include "hoops/hoops_limits.h"
 #include "hoops/hoops.h"
 #include "hoops/hoops_pil_factory.h"
 #include "hoops/hoops_pil.h"
 
-int main(int argc, char* argv[]) {
+int main(int iargc, char* argv[]) {
    std::string filename("file.par");
+
+// Replace argv[0] with this filename
+   delete argv[0];
+   argv[0] = strdup(filename.c_str());
+
    hoops::IParFile * pf 
-      = hoops::PILParFileFactory().NewIParFile(filename.c_str());
+      = hoops::PILParFileFactory().NewIParFile(argv[0]);
 
    pf->Load();  // should be in constructor?
 
-   char *lame_char_array[1];
-   lame_char_array[0] = strdup(filename.c_str());
    hoops::IParPrompt * prompter 
-      = hoops::PILParPromptFactory().NewIParPrompt(1, lame_char_array);
+      = hoops::PILParPromptFactory().NewIParPrompt(iargc, argv);
 
    prompter->Prompt();
 
    hoops::IParGroup & pg = prompter->Group();
 
-   string my_string = pg["string_par"];
+   std::string my_string = pg["string_par"];
    double my_double = pg["double_par"];
 
    std::cout << my_string << "\n"
              << my_double << std::endl;
 
+   pf->Group() = pg;
    pf->Save();   // should be in destructor?
 
    delete pf;
