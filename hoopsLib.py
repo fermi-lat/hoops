aa06298cb1fe072b5db0ff3fb7ee405e7e93915f
golpa <>
2008-02-04 17:15:42 +0000

    Use APE in SCons instead of pil

--- hoopsLib.py
+++ hoopsLib.py
@@ -1,6 +1,6 @@
 def generate(env, **kw):
 	env.Tool('addLibrary', library = ['hoops'], package = 'hoops')
-	env.Tool('addLibrary', library = env['pilLibs'])
+	env.Tool('addLibrary', library = env['apeLibs'])
 
 def exists(env):
 	return 1
